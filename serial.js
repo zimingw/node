2114d7b149127a99dc82ae788e63c95f6d1861c6
Ziming Wang <zimingwang@192-168-1-6.tpgi.com.au>
2015-12-10 08:40:34 +1100

    add sending and receiving

--- serial.js
+++ serial.js
@@ -1,6 +1,19 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor 
  
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+    // note:  d is an object, and when converted to a string it will
+    // end with a linefeed.  so we (rather crudely) account for that  
+    // with toString() and then substring() 
+    console.log("you entered: [" + 
+        d.toString().trim() + "]");
+    sp.write(d, function(){
+    	console.log("sent: " + d);
+    });
+  });
+
 var sp = new SerialPort("/dev/tty.usbserial-A50285BI", {
 	baudrate: 115200,
   	parser: serialport.parsers.readline("\n")
