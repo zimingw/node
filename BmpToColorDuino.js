d7b29e805fe2913af703e9ee9062a96938a6c2db
Ziming Wang <zwang@atlassian.com>
2016-01-10 15:33:39 +1100

    handle wildcard

--- BmpToColorDuino.js
+++ BmpToColorDuino.js
@@ -2,6 +2,8 @@
 var pngparser = require("pngparse");
 var color = require("cli-color");
 var SerialPort = require("serialport").SerialPort;
+var glob = require("glob");
+var sleep = require("sleep");
 
 //Constants
 var startByte = 0x10;
@@ -46,55 +48,70 @@ function sendImageViaSerial(arrayToSend)
 	 });
 };
 
+function sendFile(imgFile)
+{
+	pngparser.parseFile(imgFile, function(err, data){
+		if(err)
+			throw err;
+
+		function printImage(imgArray, colorFunc){
+			var index = 1;
+			for(var j = 0; j < 8; j++)
+			{
+				var row = "";
+				for(var i = 0; i < 8; i++)
+				{
+					var pixel = "[" + imgArray[j*8 + i] + "]";
+					row = row + pixel;
+				}
+				console.log(colorFunc(row));	
+			}
+		};
+
+		function extractRGB()
+		{
+			for(var j = 0; j < data.data.length; j++)
+			{
+				if ((j + 4) % 4 == 0)//red channel
+					redArray.push(data.data[j]);
+				else if ((j + 3) % 4 == 0)//green channel
+					greenArray.push(data.data[j]);
+				else if ((j + 2) % 4 == 0)//blue channel
+					blueArray.push(data.data[j]);
+			}
+			printImage(redArray, color.red);
+			printImage(greenArray, color.green);
+			printImage(blueArray, color.blue);
+		}
+
+		function sendBuffer(colorByte, colorArray)
+		{
+			var sendBuffer = new Array();
+			sendBuffer = buildSendBuffer(sendBuffer, colorByte, colorArray);	
+			sendImageViaSerial(sendBuffer);	
+		}
+
+		extractRGB();
+		sendBuffer(redByte, redArray);
+		sendBuffer(greenByte, greenArray);
+		sendBuffer(blueByte, blueArray);
+	});
+}
+
 var imgFile = process.argv[2];
 console.log("Loading image file: " + imgFile);
-//Load bmp file pixels...
-pngparser.parseFile(imgFile, function(err, data){
-	if(err)
-		throw err;
 
-	function printImage(imgArray, colorFunc){
-		var index = 1;
-		for(var j = 0; j < 8; j++)
-		{
-			var row = "";
-			for(var i = 0; i < 8; i++)
-			{
-				var pixel = "[" + imgArray[j*8 + i] + "]";
-				row = row + pixel;
-			}
-			console.log(colorFunc(row));	
-		}
-	};
-
-	function extractRGB()
-	{
-		for(var j = 0; j < data.data.length; j++)
-		{
-			if ((j + 4) % 4 == 0)//red channel
-				redArray.push(data.data[j]);
-			else if ((j + 3) % 4 == 0)//green channel
-				greenArray.push(data.data[j]);
-			else if ((j + 2) % 4 == 0)//blue channel
-				blueArray.push(data.data[j]);
-		}
-		printImage(redArray, color.red);
-		printImage(greenArray, color.green);
-		printImage(blueArray, color.blue);
+glob(imgFile + "*.png", null, function (err, files){
+	console.log(files);
+	if(files.length == 1){
+		console.log("find one file only.");
+		sendFile(files[0]);
 	}
-
-	function sendBuffer(colorByte, colorArray)
-	{
-		var sendBuffer = new Array();
-		sendBuffer = buildSendBuffer(sendBuffer, colorByte, colorArray);	
-		sendImageViaSerial(sendBuffer);	
+	else{
+		console.log("find multiple file, let's create an animation.");
+		for (var i=0; i<files.length; i++){
+			sendFile(files[i]);
+			sleep.sleep(1);
+		}
 	}
-
-	extractRGB();
-	sendBuffer(redByte, redArray);
-	sendBuffer(greenByte, greenArray);
-	sendBuffer(blueByte, blueArray);
 });
-
-
-///////////////////////////////////////////////////////////////////////
